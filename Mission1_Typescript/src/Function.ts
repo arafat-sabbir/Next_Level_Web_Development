4d9ae324db73cdbecae8eb0fa8affd8638816569
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-01 01:25:56 +0600

    Arrow Function on Array Method

--- Mission1_Typescript/src/Function.ts
+++ Mission1_Typescript/src/Function.ts
@@ -1,12 +1,15 @@
 function sum(a: number, b: number = 10): number {
   return a + b;
 }
-const addValue = (a: number=10, b: number): number =>  a + b;
+const addValue = (a: number = 10, b: number): number => a + b;
 
 const user = {
-    name:"Arafat Sabbir",
-    balance:20,
-    addBalance:function(newBalance:number):number{
-        return this.balance+newBalance;
-    }
-}
+  name: "Arafat Sabbir",
+  balance: 20,
+  addBalance: function (newBalance: number): number {
+    return this.balance + newBalance;
+  },
+};
+
+const arr = [10, 20, 30];
+const doubleArray: number[] = arr.map((item: number): number => item * item);
