cba5bb820a2ba1b7552e0e33575d9328e9191c37
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-01 00:54:24 +0600

    Explored Optional Types Readonly And Literal Type

--- Mission1_Typescript/src/BasicTypes.ts
+++ Mission1_Typescript/src/BasicTypes.ts
@@ -1,16 +1,22 @@
-let myName: string = "Arafat Sabbir";
-let myRoll: number = 786349;
-let isMarried: boolean = false;
-let isSingle: undefined = undefined;
-let marriageDate: null = null;
-
+// Primitive
+let myName: string = "Arafat Sabbir"; // string
+let myRoll: number = 786349; // number
+let isMarried: boolean = false; // boolean
+let isSingle: undefined = undefined; // undefined
+let marriageDate: null = null; // null
+// Non Primitive
 let favoriteLanguage: string[] = [
+  // array of strings
   "c",
   "c++",
   "python",
   "javascript",
   "typescript",
 ];
-let numberOfAnime: number[] = [10, 20, 30, 40, 50];
-let myBiodata: [string, number, boolean] = ["Sabbir", 19, false];
-let myList: object = { first: "fish", second: "vegetable", third: "milk" };
+let numberOfAnime: number[] = [10, 20, 30, 40, 50]; // array of numbers
+let myBiodata: [string, number, boolean] = ["Sabbir", 19, false]; // tuple with string, number, boolean types
+let myList: {
+  readonly first: string; //Readonly Types
+  second?: string; //optional Types
+  third: "milk"; //literal Types
+} = { first: "fish", third: "milk" }; // object
