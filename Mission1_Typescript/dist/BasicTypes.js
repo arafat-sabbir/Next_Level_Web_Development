dd65faacc5ca609438a76c974cd0013fc58a6843
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-30 19:40:59 +0600

    Explored Basic DataTypes Of Typescript

---
+++ Mission1_Typescript/dist/BasicTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+let myName = "Arafat Sabbir";
+let myRoll = 786349;
+let isMarried = false;
+let isSingle = undefined;
+let marriageDate = null;
+let favoriteLanguage = [
+    "c",
+    "c++",
+    "python",
+    "javascript",
+    "typescript",
+];
+let numberOfAnime = [10, 20, 30, 40, 50];
+let myBiodata = ["Sabbir", 19, false];
+let myList = { first: "fish", second: "vegetable", third: "milk" };
---
+++ Mission1_Typescript/src/BasicTypes.ts
@@ -0,0 +1,16 @@
+let myName: string = "Arafat Sabbir";
+let myRoll: number = 786349;
+let isMarried: boolean = false;
+let isSingle: undefined = undefined;
+let marriageDate: null = null;
+
+let favoriteLanguage: string[] = [
+  "c",
+  "c++",
+  "python",
+  "javascript",
+  "typescript",
+];
+let numberOfAnime: number[] = [10, 20, 30, 40, 50];
+let myBiodata: [string, number, boolean] = ["Sabbir", 19, false];
+let myList: object = { first: "fish", second: "vegetable", third: "milk" };
