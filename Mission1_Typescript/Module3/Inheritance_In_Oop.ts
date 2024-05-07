fb312741dd5eb67b2d748549ec224ee9dd4380ff
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-07 21:13:26 +0600

    Type Guard Using Type Of Operator

---
+++ Mission1_Typescript/Module3/Inheritance_In_Oop.ts
@@ -0,0 +1,13 @@
+{
+  class Person {
+    name: string;
+    weight: number;
+    height: number;
+    constructor(name: string, weight: number, height: number){
+        this.name= name;
+        this.weight = weight;
+        this.height = height;
+    }
+    
+  }
+}
