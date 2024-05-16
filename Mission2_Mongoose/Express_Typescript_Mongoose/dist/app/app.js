f98f4d28db054af809b2a3622bac3b906bb4faf4
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-17 00:44:48 +0600

    Explored Parsers, Request And Response Object

--- Mission2_Mongoose/Express_Typescript_Mongoose/dist/app/app.js
+++ Mission2_Mongoose/Express_Typescript_Mongoose/dist/app/app.js
@@ -5,7 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.text());
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.json("Hello there");
 });
 exports.default = app;
--- Mission2_Mongoose/Express_Typescript_Mongoose/src/app/app.ts
+++ Mission2_Mongoose/Express_Typescript_Mongoose/src/app/app.ts
@@ -1,7 +1,18 @@
-import express from "express";
+import express, { Response, Request } from "express";
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.text())
+
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+  
 });
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json("Hello there")
+});
+
 export default app;
