b9d8900955d53eb82df516e0b0516b2aabce2243
arafat-sabbir <arafatshabbir8@gmail.com>
2024-05-29 03:34:10 +0600

    Created Controller For Academic Department

---
+++ Mission2_Mongoose/University Management/src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -0,0 +1,35 @@
+import sendResponse from '../../../app/utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import { AcademicDepartmentServices } from './academicDepartment.service';
+
+const createAcademicDepartment = catchAsync(async (req, res) => {
+  const result = await AcademicDepartmentServices.createAcademicDepartmentIntoDb(req.body);
+  sendResponse(res, { message: 'Academic Department Created Successfully', data: result });
+});
+
+const getAllAcademicDepartment = catchAsync(async (req, res) => {
+  const result = await AcademicDepartmentServices.getAllAcademicDepartmentFromDb();
+  sendResponse(res, { message: 'Academic Department Retrieved Successfully', data: result });
+});
+const getSingleAcademicDepartment = catchAsync(async (req, res) => {
+  const result = await AcademicDepartmentServices.getSingleAcademicDepartmentFromDb(req.params.DepartmentId);
+  sendResponse(res, { message: 'Academic Department Retrieved Successfully', data: result });
+});
+
+const updateSingleAcademicDepartment = catchAsync(async (req, res) => {
+  const result = await AcademicDepartmentServices.updateSingleAcademicDepartmentFromDb(
+    req.params.DepartmentId,
+    req.body
+  );
+  sendResponse(res, {
+    message: `Department For ${req.params.semesterId} Updated Successfully`,
+    data: result,
+  });
+});
+
+export const AcademicDepartmentControllers = {
+  createAcademicDepartment,
+  getSingleAcademicDepartment,
+  getAllAcademicDepartment,
+  updateSingleAcademicDepartment,
+};
