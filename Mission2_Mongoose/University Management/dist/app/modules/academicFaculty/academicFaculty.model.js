9183e6225bfff4b89c6c4d5bcae5e8b65f37330e
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-04 12:09:46 +0600

    Deleted Dist File From Github

--- Mission2_Mongoose/University Management/dist/app/modules/academicFaculty/academicFaculty.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const academicFacultySchema = new mongoose_1.Schema({
-    name: { type: String, required: true, unique: true },
-}, { timestamps: true });
-const AcademicFacultyModel = (0, mongoose_1.model)('academicFaculties', academicFacultySchema);
-exports.default = AcademicFacultyModel;
