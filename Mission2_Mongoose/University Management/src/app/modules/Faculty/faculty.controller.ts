1fc88df86149a03748e3c190d22389ce8ebd2102
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-04 19:36:53 +0600

    Added Faculty And Admin Modules From Practice Task

---
+++ Mission2_Mongoose/University Management/src/app/modules/Faculty/faculty.controller.ts
@@ -0,0 +1,51 @@
+import catchAsync from 'src/app/utils/catchAsync';
+import { FacultyServices } from './faculty.service';
+import sendResponse from 'src/app/utils/sendResponse';
+
+const getSingleFaculty = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await FacultyServices.getSingleFacultyFromDB(id);
+
+  sendResponse(res, {
+    message: 'Faculty is retrieved Successfully',
+    data: result,
+  });
+});
+
+const getAllFaculties = catchAsync(async (req, res) => {
+  const result = await FacultyServices.getAllFacultiesFromDB(req.query);
+
+  sendResponse(res, {
+    message: 'Faculties are retrieved Successfully',
+    data: result,
+  });
+});
+
+const updateFaculty = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const { faculty } = req.body;
+  const result = await FacultyServices.updateFacultyIntoDB(id, faculty);
+
+  sendResponse(res, {
+    message: 'Faculty is updated Successfully',
+    data: result,
+  });
+});
+
+const deleteFaculty = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await FacultyServices.deleteFacultyFromDB(id);
+
+  sendResponse(res, {
+    message: 'Faculty is deleted Successfully',
+    data: result,
+  });
+});
+
+export const FacultyControllers = {
+  getAllFaculties,
+  getSingleFaculty,
+  deleteFaculty,
+  updateFaculty,
+};
+
