d1364b01cb0830d66d4fa3b1da885c3d980d85ac
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-06 23:36:59 +0600

    Added Jsdocs On CourseModel

--- Mission2_Mongoose/University Management/src/app/modules/course/course.model.ts
+++ Mission2_Mongoose/University Management/src/app/modules/course/course.model.ts
@@ -1,18 +1,31 @@
 import { Schema, model } from 'mongoose';
 import { TCourse, TPreRequisiteCourse } from './course.interface';
 
-const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourse>({
-  course: { type: Schema.Types.ObjectId, ref: 'Course', required: true },
-  isDeleted: { type: Boolean, default: false },
-});
-
+/**
+ * The schema for a course.
+ * @property {string} title - The title of the course.
+ * @property {string} prefix - The prefix of the course.
+ * @property {number} code - The code of the course.
+ * @property {number} credit - The credit of the course.
+ * @property {Array<TPreRequisiteCourse>} preRequisiteCourses - The pre-requisite courses.
+ */
 const courseSchema = new Schema<TCourse>({
   title: { type: String, required: true, unique: true },
   prefix: { type: String, required: true },
   code: { type: Number, required: true },
   credit: { type: Number, required: true },
-  preRequisiteCourses: [preRequisiteCoursesSchema],
+  preRequisiteCourses: [
+    new Schema<TPreRequisiteCourse>({
+      course: { type: Schema.Types.ObjectId, ref: 'Course', required: true },
+      isDeleted: { type: Boolean, default: false },
+    }),
+  ],
 });
 
+/**
+ * The model for a course.
+ * @type {Model<TCourse>}
+ */
 const CourseModel = model<TCourse>('Course', courseSchema);
 export default CourseModel;
+
