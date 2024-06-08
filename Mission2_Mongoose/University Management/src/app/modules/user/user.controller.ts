be55bb84ed29c57fea5e88b450184e48812cfd29
arafat-sabbir <arafatshabbir8@gmail.com>
2024-06-08 15:49:49 +0600

    Created Controller To Add New Faculty And Admin

--- Mission2_Mongoose/University Management/src/app/modules/user/user.controller.ts
+++ Mission2_Mongoose/University Management/src/app/modules/user/user.controller.ts
@@ -7,5 +7,15 @@ const createNewStudent = catchAsync(async (req, res) => {
   const result = await userService.createStudentOnDb(password, studentData);
   sendResponse(res, { message: 'Student Created Successfully', data: result });
 });
+const createNewFaculty = catchAsync(async (req, res) => {
+  const { password, faculty: studentData } = req.body;
+  const result = await userService.createFacultyIntoDB(password, studentData);
+  sendResponse(res, { message: 'Faculty Created Successfully', data: result });
+});
+const createNewAdmin = catchAsync(async (req, res) => {
+  const { password, admin: studentData } = req.body;
+  const result = await userService.createAdminIntoDB(password, studentData);
+  sendResponse(res, { message: 'Admin Created Successfully', data: result });
+});
 
-export const userControllers = { createNewStudent };
+export const userControllers = { createNewStudent, createNewFaculty, createNewAdmin };
